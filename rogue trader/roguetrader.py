8053f465fc14b83cd292df0a50a63779b30c0195
Ziemowit Musiatowicz <musiatowiczz@gmail.com>
2024-08-25 19:49:10 +0200

    rogue trader

---
+++ rogue trader/roguetrader.py
@@ -0,0 +1,72 @@
+from talon import Module, Context
+
+mod = Module()
+mod.apps.wh40k_rogue_trader = r"""
+app.name: WH40KRT.exe
+and app.exe: /^wh40krt\.exe$/i
+"""
+
+mod.list("wh40k_rogue_trader_character_shortcuts")
+mod.list("wh40k_rogue_trader_consumable_shortcuts")
+mod.list("wh40k_rogue_trader_weapon_shortcuts")
+mod.list("wh40k_rogue_trader_ability_shortcuts")
+
+ctx = Context()
+ctx.matches = """
+mode: user.game
+and app: wh40k_rogue_trader
+"""
+
+ctx.lists["user.wh40k_rogue_trader_character_shortcuts"] = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+}
+
+ctx.lists["user.wh40k_rogue_trader_consumable_shortcuts"] = {
+    "one": "f1",
+    "two": "f2",
+    "three": "f3",
+    "four": "f4",
+}
+
+ctx.lists["user.wh40k_rogue_trader_weapon_shortcuts"] = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+    "zero": "0",
+    "eleven": "-",
+    "twelve": "=",
+}
+
+ctx.lists["user.wh40k_rogue_trader_ability_shortcuts"] = {
+    "one": "shift-1",
+    "two": "shift-2",
+    "three": "shift-3",
+    "four": "shift-4",
+    "five": "shift-5",
+    "six": "shift-6",
+    "seven": "shift-7",
+    "eight": "shift-8",
+    "nine": "shift-9",
+    "zero": "shift-0",
+    "eleven": "ctrl-1",
+    "twelve": "ctrl-2",
+    "thirteen": "ctrl-3",
+    "fourteen": "ctrl-4",
+    "fifteen": "ctrl-5",
+    "sixteen": "ctrl-6",
+    "seventeen": "ctrl-7",
+    "eighteen": "ctrl-8",
+    "nineteen": "ctrl-9",
+    "twenty": "ctrl-0",
+}
--- rogue trader/roguetrader.talon
+++ rogue trader/roguetrader.talon
@@ -1,15 +1,38 @@
-mode: user.game
-and app.name: WH40KRT.exe
-and app.exe: /^wh40krt\.exe$/i
+app: wh40k_rogue_trader
+and not mode: sleep
 -
+tag(): user.game_genre_crpg
+tag(): user.game_camera_keyboard_controls
+tag(): user.game_camera_zoom
+tag(): user.game_map
 
-settings():
-	user.mouse_continuous_scroll_amount = 250
-	user.mouse_wheel_down_amount = 250
-	user.mouse_enable_pop_click = 0
-	user.mouse_hide_mouse_gui = 1
-	key_hold = 64.0
-	key_wait = 16.0
-	user.game_noise_pop_binding_default = "click"
-	user.game_noise_hiss_binding_default = "right long click"
-	user.game_minimum_hiss_duration = "180ms"
+(encyclopedia | index) show: key(l)
+colony [management] show: key(y)
+cargo [management] show: key(b)
+[void] ship go: key(v)
+(formation | form) show: key(n)
+
+stop | top: key(g)
+position hold | hold up | relax: key(h)
+cam follow car: key(f)
+menu mod show: key(shift-f10)
+you I (shob | hide): key(ctrl-f11)
+
+log show: key(p)
+tab last: key(shift-q)
+tab next: key(shift-e)
+car last: key(shift-a)
+car next: key(shift-d)
+
+car {user.wh40k_rogue_trader_character_shortcuts}:
+    key("alt-{wh40k_rogue_trader_character_shortcuts}")
+car take all: key(backspace)
+
+dialog end: key(space)
+weapon switch: key(x)
+weapon {user.wh40k_rogue_trader_weapon_shortcuts}:
+    key("{wh40k_rogue_trader_weapon_shortcuts}")
+consume {user.wh40k_rogue_trader_consumable_shortcuts}:
+    key("{wh40k_rogue_trader_consumable_shortcuts}")
+(ability | skill | kill) {user.wh40k_rogue_trader_ability_shortcuts}:
+    key("{wh40k_rogue_trader_ability_shortcuts}")
